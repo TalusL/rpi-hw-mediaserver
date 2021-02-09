3f23512175609d205d3d72ea9f8b75b3bbd5ce3b
ziyaofeng <xiaoxiaochenjian@gmail.com>
2021-02-09 17:54:56 +0800

    ADD MEDIA SERVER

--- main.cpp
+++ main.cpp
@@ -5,6 +5,9 @@
 #include "MediaServer.h"
 
 int main(){
+    //set logger
+    Logger::Instance().add(std::make_shared<ConsoleChannel>("ConsoleChannel", LTrace));
+
     //start media server
     MediaServer::Ptr server = make_shared<MediaServer>();
     server->start();
@@ -15,4 +18,5 @@ int main(){
         InfoL << "SIGINT:exit";
         exit(0);
     });
+    sem.wait();
 }
